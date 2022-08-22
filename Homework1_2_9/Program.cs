96de55c3169ecb667588dfc8aeab080eb2c34b02
PavelOotsutsuki <109985069+PavelOotsutsuki@users.noreply.github.com>
2022-08-22 11:38:42 +0300

    Добавьте файлы проекта.

---
+++ Homework1_2_9/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework1_2_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int rangeNumberBegin = 1;
+            int rangeNumberEnd = 27;
+            int rangeThreeDigitNumbersBegin = 100;
+            int rangeThreeDigitNumbersEnd = 999;
+            int countDivisible = 0;
+            int number = random.Next(rangeNumberBegin , rangeNumberEnd +1);
+            int potentialDivisible;
+
+            for (int i = rangeThreeDigitNumbersBegin; i <= rangeThreeDigitNumbersEnd; i++)
+            {
+                potentialDivisible = i;
+
+                while (potentialDivisible>0)
+                {
+                    potentialDivisible-=number;
+                }
+
+                if (potentialDivisible==0)
+                {
+                    countDivisible++;
+                }
+            }
+
+            Console.WriteLine("У числа " + number + " количество кратных чисел равно " + countDivisible);
+        }
+    }
+}
